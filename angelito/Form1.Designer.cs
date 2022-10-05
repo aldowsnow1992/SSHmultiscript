26fa3a06a380a68578fb9690f1c3da462259b1d5
AGUIRRE TORRES BAYRON IVAN <BAGUIRRE@sky.com.mx>
2022-10-05 18:32:59 -0500

    Agregar archivos de proyecto.

---
+++ angelito/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace angelito
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.btniniciar = new System.Windows.Forms.Button();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.IPs = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(56, 206);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(1142, 532);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // btniniciar
+            // 
+            this.btniniciar.Location = new System.Drawing.Point(239, 94);
+            this.btniniciar.Name = "btniniciar";
+            this.btniniciar.Size = new System.Drawing.Size(112, 34);
+            this.btniniciar.TabIndex = 1;
+            this.btniniciar.Text = "Inyectar";
+            this.btniniciar.UseVisualStyleBackColor = true;
+            this.btniniciar.Click += new System.EventHandler(this.btniniciar_Click);
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(626, 81);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(86, 58);
+            this.btnagregar.TabIndex = 2;
+            this.btnagregar.Text = "agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.IPs);
+            this.groupBox1.Controls.Add(this.btniniciar);
+            this.groupBox1.Location = new System.Drawing.Point(789, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(392, 187);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server a revisar";
+            // 
+            // IPs
+            // 
+            this.IPs.FormattingEnabled = true;
+            this.IPs.ItemHeight = 25;
+            this.IPs.Location = new System.Drawing.Point(18, 30);
+            this.IPs.Name = "IPs";
+            this.IPs.Size = new System.Drawing.Size(182, 154);
+            this.IPs.TabIndex = 2;
+            this.IPs.DoubleClick += new System.EventHandler(this.IPs_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 31);
+            this.textBox2.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.btnagregar);
+            this.groupBox2.Location = new System.Drawing.Point(56, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(727, 195);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Agregar servers";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Comando";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(239, 147);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(345, 31);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "User";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(239, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(251, 31);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 750);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.rtb);
+            this.Name = "Form1";
+            this.Text = "Elangelitomovil";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button btniniciar;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox IPs;
+    }
+}
+
---
+++ angelito/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci;
+using Renci.SshNet;
+
+namespace angelito
+{
+    public partial class Form1 : Form
+    {
+        List<Server> L = new List<Server>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Click(object sender, EventArgs e)
+        {
+            new Task(() => RunCommand()).Start();
+        }
+        public void testejecucion()
+        {
+            using (var client = new SshClient("hostnameOrIp", "username", "password"))
+            {
+
+            }
+        }
+
+
+
+
+        private void RunCommand()
+        {
+            var host = "192.168.0.110";
+            var username = "ubuntu";
+            var password = "M64z1992";
+
+            using (var client = new SshClient(host, username, password))
+            {
+                client.Connect();
+                var cmd = client.CreateCommand("whoami; ifconfig; whoami");
+
+                var result = cmd.BeginExecute();
+
+                using (var reader = new StreamReader(
+                                      cmd.OutputStream, Encoding.UTF8, true, 1024, true))
+                {
+                    while (!result.IsCompleted || !reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (line != null)
+                        {
+                            rtb.Invoke(
+                                (MethodInvoker)(() =>
+                                    rtb.AppendText(line + Environment.NewLine)));
+                        }
+                    }
+                }
+
+                cmd.EndExecute(result);
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            Server s = new Server();
+            s.IPP = textBox1.Text;
+            s.USER = textBox2.Text;
+            s.CONTR= textBox3.Text;
+            s.COMAN = textBox4.Text;
+            L.Add(s);
+            IPs.Items.Clear();
+            foreach (var item in L)
+            {
+                IPs.Items.Add(item.IPP);
+            }
+
+        }
+
+        private void IPs_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show( "eliminado " + IPs.SelectedItem.ToString());
+
+            var d = L.FirstOrDefault(x=> x.IPP== IPs.SelectedItem.ToString());
+            L.Remove(d);
+            IPs.Items.Clear();
+            foreach (var item in L)
+            {
+                IPs.Items.Add(item.IPP);
+            }
+
+        }
+    }//fin
+    public class Server
+    {
+        public string IPP { get; set; }
+        public string USER { get; set; }
+        public string CONTR { get; set; }
+        public string COMAN { get; set; }
+
+
+
+       
+    }
+
+    public class info
+        {
+        public static string ipdata;
+        public static string  userdata;
+        public static string passdata;
+        public static string data;
+    }
+    
+}
